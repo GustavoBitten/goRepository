d531533556a6435b9a3bcfe86e6680b3054a1ecf
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-06-15 23:49:41 -0300

    Add functionality create, index and modify

--- src/app.js
+++ src/app.js
@@ -1,32 +1,82 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+var repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+
+
+class Repository {
+  constructor(id,title,url,techs,likes = 0){
+    this.id = id
+    this.title = title
+    this.url = url
+    this.techs = techs
+    this.likes = likes
+
+  }
+}
+
+app.get("/repositories", (req, res) => {
+
+  res.status(200).json(repositories)
+
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+
+  const id = uuid()
+  const {title,url,techs} = req.body
+
+  const repository = new Repository(id,title,url,techs)
+
+  res.status(201).json(repository)
+
+  return repositories.push(repository)
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  
+  const {id} = req.params
+  const {title,url,techs} = req.body
+
+  let changeHappened = false 
+
+  repositories = repositories.map((repository)=>{
+
+    if(repository.id == id ){
+      
+      repository.title = title
+      repository.url = url
+      repository.techs = techs
+      changeHappened = true
+    }
+    return repository
+  })
+
+  if(changeHappened){
+    res.status(202).json(repositories)
+  }else{
+    res.status(400).json({error: 'this repository not exist'})
+  }
+
+  
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  
+  
+
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", (req, res) => {
   // TODO
 });
 
